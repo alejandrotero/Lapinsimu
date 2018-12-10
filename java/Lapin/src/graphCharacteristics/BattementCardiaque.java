160ac07e4f4d5c7653f20461c431b7bdd502072b
zazazelle57 <32510265+zazazelle57@users.noreply.github.com>
2018-12-10 20:50:09 +0100

    Create BattementCardiaque.java

---
+++ java/Lapin/src/graphCharacteristics/BattementCardiaque.java
@@ -0,0 +1,21 @@
+package graphCharacteristics;
+
+
+public class BattementCardiaque {
+	
+	void GaussValues() {
+		double[] valeursX =  new double[50];
+		double[] valeursY =  new double[50];
+		double s=0;
+		for (int i=0; i<50; i++) {
+			valeursX[i]=s;
+			valeursY[i]=Math.
+			s=s+0.1;
+		}
+	}
+	
+	public static void main(String[] argv) {
+		
+	}
+
+}
