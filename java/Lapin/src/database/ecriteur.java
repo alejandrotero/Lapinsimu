603702d2926aa3c8fd6a3287f8237332dbbe0bf1
Guliver <guillaume.colsenet@free.fr>
2019-01-16 16:16:47 +0100

    [creation et ecriture correcte]

--- java/Lapin/src/database/ecriteur.java
+++ java/Lapin/src/database/ecriteur.java
@@ -3,158 +3,94 @@ import java.net.HttpURLConnection;
 
 import java.net.*;
 import java.io.*;
+import java.net.HttpURLConnection;
 
 public class ecriteur {
-        private static HttpURLConnection client;
-    URL input;
 
+public void ecrire(String nomdb, int valeur,int time){
+     String POST_URL2 = "http://localhost:8086/write?db="+nomdb;
+     String POST_PARAMS2 = "cpu_load_short,host=server01,region=us-west value=0.64 1434055572000000000";
 
-
-    public boolean ecrire(int valeur,int time){
-        try {
-            input = new URL("http", "localhost", 8086, "/write?db=mydb");
-            client = (HttpURLConnection) input.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
-
-            //double thermals = rn.nextDouble();
-            String s = "cpu_temperature value=" + 600000000;
-
-            try (OutputStreamWriter writer =
-                    new OutputStreamWriter(client.getOutputStream())) {
-                writer.write(s);
-            }
-
-            return true;
-        }catch(MalformedURLException error) {
-            System.out.println("Malformed URL!");
-            return false;
-
-    }catch(SocketTimeoutException error) {
-        System.out.println("Socket Timeout Exception!");
-        return false;
-
-    } catch (IOException error) {
+     //String POST_PARAMS2 = "timey= "+time+", value="+valeur+", 1434055562000000000";
+    try{
+        sendPOST(POST_PARAMS2 , POST_URL2);
+    }catch (IOException error) {
         System.out.println("IOException!");
         System.out.println(error);
-        return false;
+    }
+        
+}
+
+   
+public String lire(String nomdb){
+     String POST_URL3 = "http://localhost:8086/query?db="+nomdb; 
+     /*"
+     (`
+                select * from pression
+                where host = ${Influx.escape.stringLit(os.hostname())}
+                order by time desc
+                limit 1
+              `)
+"*/
+     
+     String POST_PARAMS3 = "q=SELECT \"*\" FROM \"pression\" ORDER BY \"time\" DESC LIMIT 1 ";
+    try{
+        return sendPOST(POST_PARAMS3 , POST_URL3);
+    }catch (IOException error) {
+        System.out.println("IOException!");
+        return error.toString();
     }
 
 }
 
-public void ecrire2(int valeur,int time){
-    try {
-        URL input;
-        
-            input = new URL("http", "localhost", 8086, "/write?db=mydbbi");
-            client = (HttpURLConnection) input.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
-
-            double thermals = valeur;//rn.nextDouble();
-            String s = "cpu_temperature value=" + thermals;
-
-            try (OutputStreamWriter writer =
-                    new OutputStreamWriter(client.getOutputStream())) {
-                writer.write(s);
-            }
-            //*
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                                        client.getInputStream()));
-            String decodedString;
-            while ((decodedString = in.readLine()) != null) {
-                System.out.println(decodedString);
-            }
-            in.close();
-            //*/
-        System.out.println(s); // for debugging
-        
-
-        } catch(MalformedURLException error) {
-            System.out.println("Malformed URL!");
-        } catch(SocketTimeoutException error) {
-            System.out.println("Socket Timeout Exception!");
-        } catch (IOException error) {
-            System.out.println("IOException!");
-            System.out.println(error);
-       // } catch(InterruptedException e) {
-       //     System.out.println("InterruptedException!");
-        } finally {
-            if(client != null) { // Make sure the connection is not null.
-                client.disconnect();
-            }
-        }
-}
-
-public String lire(){
-    try {
-        
-            input = new URL("http", "localhost", 8086, "/write?db=mydb");
-            client = (HttpURLConnection) input.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
-            //*
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                                        client.getInputStream()));
-            String decodedString;
-            decodedString = in.readLine();
-            //return in.readLine();
-            //*
-            while (decodedString != null) {
-                System.out.println(decodedString);
-                return decodedString;
-            }
-            //*/ 
-            in.close();        
-
-        } catch(MalformedURLException error) {
-            System.out.println("Malformed URL!");
-            return "Malformed URL!";
-        } catch(SocketTimeoutException error) {
-            System.out.println("Socket Timeout Exception!");
-            return "Socket Timeout Exception!";
-        } catch (IOException error) {
-            System.out.println("IOException!");
-            System.out.println(error);
-            return "IOException!";
-       // } catch(InterruptedException e) {
-       //     System.out.println("InterruptedException!");
-        } finally {
-            if(client != null) { // Make sure the connection is not null.
-                client.disconnect();
-            }
-        }
-        return " t";
-}
-
-
-
 
 public void creerDB(String nomdb){
-    try {
-        URL input;
-         // /query --data-urlencode "q=CREATE DATABASE foofoo"
-            input = new URL("http", "localhost", 8086, "q=CREATE DATABASE "+nomdb);
-            client = (HttpURLConnection) input.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
 
-        
+	String POST_URL = "http://localhost:8086/query";
+    String POST_PARAMS = "q=CREATE DATABASE "+nomdb;
+    try{
+        sendPOST(POST_PARAMS , POST_URL);
+    }catch (IOException error) {
+        System.out.println("IOException!");
+        System.out.println(error);
+    }
+}
 
-        } catch(MalformedURLException error) {
-            System.out.println("Malformed URL!");
-        } catch(SocketTimeoutException error) {
-            System.out.println("Socket Timeout Exception!");
-        } catch (IOException error) {
-            System.out.println("IOException!");
-            System.out.println(error);
-       // } catch(InterruptedException e) {
-       //     System.out.println("InterruptedException!");
-        } finally {
-            if(client != null) { // Make sure the connection is not null.
-                client.disconnect();
-            }
+
+private static String sendPOST(String param,String urlvrai) throws IOException {
+    URL obj = new URL(urlvrai);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    con.setRequestMethod("POST");
+
+    // For POST only - START
+    con.setDoOutput(true);
+    OutputStream os = con.getOutputStream();
+    os.write(param.getBytes());
+    os.flush();
+    os.close();
+    // For POST only - END
+
+    int responseCode = con.getResponseCode();
+    System.out.println("POST Response Code :: " + responseCode);
+
+    if (responseCode == HttpURLConnection.HTTP_OK) { //success
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
+        in.close();
+
+        // print result
+        System.out.println(response.toString());
+        return response.toString();
+    } else {
+        System.out.println("POST request not worked");
+        return "error";
+    }
 }
 
 }
--- java/Lapin/src/database/ecriture.java
+++ java/Lapin/src/database/ecriture.java
@@ -12,76 +12,25 @@ utiliser plutot les fonctions ecrire et lire de ecriteur
 bisous a tous
 */
     public static void main(String[] args) {
-       /*
-        try {
-        //*/
         ecriteur e = new ecriteur();
-        for(int i = 1; i<= 10; i++){
-            
-            System.out.println(e.ecrire(10, 10));
-            System.out.println(e.lire());
-        }
-        e.creerDB("mydbbi");
-        System.out.println("1");
+        System.out.println("creation");
+        e.creerDB("mydbfinal");
 
-        e.ecrire2(20, 20);
+        System.out.println("ecriture");
+        e.ecrire("express_response_db", 10, 10);
+
+        System.out.println("lecture");
+        e.lire("express_response_db");
+        System.out.println("fin");
+
 
             /*
-`
+            `
                 select * from pression
                 where host = ${Influx.escape.stringLit(os.hostname())}
                 order by time desc
                 limit 1
               `)
-
-            */
-            /*
-
-            Random rn = new Random();
-            URL input;
-            while(true) {
-                input = new URL("http", "localhost", 8086, "/write?db=mydb");
-                client = (HttpURLConnection) input.openConnection();
-                client.setRequestMethod("POST");
-                client.setDoOutput(true);
-
-                double thermals = rn.nextDouble();
-                String s = "cpu_temperature value=" + thermals;
-
-                try (OutputStreamWriter writer =
-                        new OutputStreamWriter(client.getOutputStream())) {
-                    writer.write(s);
-                }
-                /*
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                                            client.getInputStream()));
-                String decodedString;
-                while ((decodedString = in.readLine()) != null) {
-                    System.out.println(decodedString);
-                }
-                in.close();
-                */
-                /*
-                System.out.println(s); // for debugging
-                Thread.sleep(1000); // send data every 1 second
-            }
-
-            
-
-        } catch(MalformedURLException error) {
-            System.out.println("Malformed URL!");
-        } catch(SocketTimeoutException error) {
-            System.out.println("Socket Timeout Exception!");
-        } catch (IOException error) {
-            System.out.println("IOException!");
-            System.out.println(error);
-        } catch(InterruptedException e) {
-            System.out.println("InterruptedException!");
-        } finally {
-            if(client != null) { // Make sure the connection is not null.
-                client.disconnect();
-            }
-        }
         //*/
 
 
