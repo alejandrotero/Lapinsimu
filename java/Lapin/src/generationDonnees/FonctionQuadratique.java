8f9b866bd683d28561e2c4e901ab3c2377b7690a
zazazelle57 <32510265+zazazelle57@users.noreply.github.com>
2019-01-23 14:53:03 +0100

    getMax

--- java/Lapin/src/generationDonnees/FonctionQuadratique.java
+++ java/Lapin/src/generationDonnees/FonctionQuadratique.java
@@ -23,7 +23,7 @@ public class FonctionQuadratique {
 		Random gen = new Random();
 		return gen.nextGaussian()*ecart+mu;
 	}
-private double generatePremierTheta(double muPourThetha1, double ecartPourTheta1,double minTheta1,double maxTheta1) {
+	private double generatePremierTheta(double muPourThetha1, double ecartPourTheta1,double minTheta1,double maxTheta1) {
 		
 		double theta1 = generateNormalRandomNumber(muPourThetha1, ecartPourTheta1);
 		while(theta1<minTheta1||theta1>maxTheta1) {
@@ -31,22 +31,31 @@ private double generatePremierTheta(double muPourThetha1, double ecartPourTheta1
 		}
 		return theta1;
 	}
-@Override
-public String toString() {
-	return "FonctionQuadratique [" +theta0 + " + " + theta1 + " x + " + theta2 + " x^2]";
-}
-/**
- * Si thetha2 = coeffA2Regression * thetha1 + coeffB2Regression
- * @param theta1
- * @param coeffA2Regression
- * @param coeffB2Regression
- * @return thetha2
- */
-
-private double generateDeuxiemeTheta(double theta1, double coeffA2Regression, double coeffB2Regression) {
-		double tetha2=(coeffA2Regression*theta1)+coeffB2Regression;
-		return tetha2;
+	@Override
+	public String toString() {
+		return "FonctionQuadratique [" +theta0 + " + " + theta1 + " x + " + theta2 + " x^2]";
 	}
+	/**
+	 * Si thetha2 = coeffA2Regression * thetha1 + coeffB2Regression
+	 * @param theta1
+	 * @param coeffA2Regression
+	 * @param coeffB2Regression
+	 * @return thetha2
+	 */
+	
+	private double generateDeuxiemeTheta(double theta1, double coeffA2Regression, double coeffB2Regression) {
+			double tetha2=(coeffA2Regression*theta1)+coeffB2Regression;
+			return tetha2;
+	}
+	
+	public double getMaxX() {
+		return (-this.theta1/(2*this.theta2));
+	}
+	
+	public double getMaxY() {
+		return (this.theta0-this.theta1*this.theta1/(2*this.theta2)+this.theta1*this.theta1/(4*this.theta2));
+	}
+	
 	public double getValue(double x) {
 		return (this.theta0+this.theta1*x+this.theta2*x*x);
 	}
